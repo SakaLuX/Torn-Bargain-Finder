f2b4c5104990fb3ad191dea5fa2510675c202141
SakaLuX <sakalutzu92@yahoo.com>
2026-08-04 16:35:31 +0300

    Update utils.js

--- src/core/utils.js
+++ src/core/utils.js
@@ -1,1 +1,43 @@
--
+/**
+ * Torn Toolkit
+ * Utils
+ */
+
+export default class Utils {
+
+    static sleep(ms) {
+
+        return new Promise(resolve => setTimeout(resolve, ms));
+
+    }
+
+    static formatNumber(number) {
+
+        return new Intl.NumberFormat().format(number);
+
+    }
+
+    static formatMoney(number) {
+
+        return "$" + this.formatNumber(number);
+
+    }
+
+    static random(min, max) {
+
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+
+    }
+
+    static create(tag, className = "") {
+
+        const el = document.createElement(tag);
+
+        if (className)
+            el.className = className;
+
+        return el;
+
+    }
+
+}
