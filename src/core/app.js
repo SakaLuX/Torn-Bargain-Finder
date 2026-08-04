7afde3b8812215a1c09b927b415e196ae617f13c
SakaLuX <sakalutzu92@yahoo.com>
2026-08-04 16:48:23 +0300

    Update app.js

--- src/core/app.js
+++ src/core/app.js
@@ -1,7 +1,7 @@
 import Logger from "./logger.js";
 import Storage from "./storage.js";
 import Router from "./router.js";
-
+import ModuleManager from "./moduleManager.js";
 import Panel from "../ui/panel.js";
 
 export default class TornToolkit {
@@ -11,7 +11,7 @@ export default class TornToolkit {
         this.version = "0.1.0";
 
         this.storage = new Storage();
-
+this.modules = new ModuleManager(this.storage);
         this.panel = new Panel(this);
 
     }
@@ -27,7 +27,41 @@ export default class TornToolkit {
         Logger.info("Current Page:", Router.current());
 
         Logger.line();
+this.modules.register({
+    id: "bargain",
+    name: "💰 Bargain Finder",
+    enabled: true
+});
 
+this.modules.register({
+    id: "travel",
+    name: "✈ Travel Tools",
+    enabled: false
+});
+
+this.modules.register({
+    id: "war",
+    name: "⚔ War Tools",
+    enabled: false
+});
+
+this.modules.register({
+    id: "crimes",
+    name: "🦹 Crimes Tools",
+    enabled: false
+});
+
+this.modules.register({
+    id: "casino",
+    name: "🎰 Casino Tools",
+    enabled: false
+});
+
+this.modules.register({
+    id: "racing",
+    name: "🏁 Racing Tools",
+    enabled: false
+});
         this.panel.render();
 
     }
