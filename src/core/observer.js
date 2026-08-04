596387277e6051caac9a5fcface504ac3b8c431b
SakaLuX <sakalutzu92@yahoo.com>
2026-08-04 16:36:13 +0300

    Update observer.js

--- src/core/observer.js
+++ src/core/observer.js
@@ -1,1 +1,47 @@
--
+/**
+ * Torn Toolkit
+ * Observer
+ */
+
+import Logger from "./logger.js";
+
+export default class Observer {
+
+    constructor(callback) {
+
+        this.callback = callback;
+
+        this.observer = null;
+
+    }
+
+    start() {
+
+        this.observer = new MutationObserver(() => {
+
+            this.callback();
+
+        });
+
+        this.observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+
+        Logger.success("MutationObserver started.");
+
+    }
+
+    stop() {
+
+        if (this.observer) {
+
+            this.observer.disconnect();
+
+            Logger.warn("MutationObserver stopped.");
+
+        }
+
+    }
+
+}
