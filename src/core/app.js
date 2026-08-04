5f0ba06bd465ecdc82f48311c6b8899d7630b79a
SakaLuX <sakalutzu92@yahoo.com>
2026-08-04 16:57:21 +0300

    Update app.js

--- src/core/app.js
+++ src/core/app.js
@@ -1,18 +1,93 @@
+/**
+ * Torn Toolkit
+ * Core Application
+ * Version: 0.2.0
+ */
+
 import Logger from "./logger.js";
 import Storage from "./storage.js";
 import Router from "./router.js";
+import Observer from "./observer.js";
 import ModuleManager from "./moduleManager.js";
+
 import Panel from "../ui/panel.js";
+import Support from "../support/support.js";
 
 export default class TornToolkit {
 
     constructor() {
 
-        this.version = "0.1.0";
+        this.version = "0.2.0";
 
-        this.storage = new Storage();
-this.modules = new ModuleManager(this.storage);
+        this.storage = new Storage("torn-toolkit");
+
+        this.logger = Logger;
+
+        this.router = Router;
+
+        this.modules = new ModuleManager(this.storage);
+
         this.panel = new Panel(this);
+
+        this.support = new Support(this);
+
+        this.observer = new Observer(() => {
+
+            // Reserved for future page updates
+
+        });
+
+    }
+
+    loadModules() {
+
+        this.modules.register({
+            id: "bargain",
+            name: "💰 Bargain Finder",
+            enabled: true
+        });
+
+        this.modules.register({
+            id: "travel",
+            name: "✈️ Travel Tools",
+            enabled: false
+        });
+
+        this.modules.register({
+            id: "war",
+            name: "⚔️ War Tools",
+            enabled: false
+        });
+
+        this.modules.register({
+            id: "crimes",
+            name: "🦹 Crimes Tools",
+            enabled: false
+        });
+
+        this.modules.register({
+            id: "casino",
+            name: "🎰 Casino Tools",
+            enabled: false
+        });
+
+        this.modules.register({
+            id: "racing",
+            name: "🏁 Racing Tools",
+            enabled: false
+        });
+
+        this.modules.register({
+            id: "inventory",
+            name: "📦 Inventory Tools",
+            enabled: false
+        });
+
+        this.modules.register({
+            id: "faction",
+            name: "👥 Faction Tools",
+            enabled: false
+        });
 
     }
 
@@ -20,49 +95,21 @@ this.modules = new ModuleManager(this.storage);
 
         Logger.line();
 
-        Logger.info("Starting Torn Toolkit");
+        Logger.success("Starting Torn Toolkit");
 
         Logger.info("Version:", this.version);
 
-        Logger.info("Current Page:", Router.current());
+        Logger.info("Page:", Router.current());
+
+        this.loadModules();
+
+        this.panel.render();
+
+        this.observer.start();
+
+        Logger.success("Toolkit Ready");
 
         Logger.line();
-this.modules.register({
-    id: "bargain",
-    name: "💰 Bargain Finder",
-    enabled: true
-});
-
-this.modules.register({
-    id: "travel",
-    name: "✈ Travel Tools",
-    enabled: false
-});
-
-this.modules.register({
-    id: "war",
-    name: "⚔ War Tools",
-    enabled: false
-});
-
-this.modules.register({
-    id: "crimes",
-    name: "🦹 Crimes Tools",
-    enabled: false
-});
-
-this.modules.register({
-    id: "casino",
-    name: "🎰 Casino Tools",
-    enabled: false
-});
-
-this.modules.register({
-    id: "racing",
-    name: "🏁 Racing Tools",
-    enabled: false
-});
-        this.panel.render();
 
     }
 
