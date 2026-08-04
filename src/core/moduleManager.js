14b9380a104e8f5aacb5288b8bfbecd9919f0b15
SakaLuX <sakalutzu92@yahoo.com>
2026-08-04 16:58:01 +0300

    Update moduleManager.js

--- src/core/moduleManager.js
+++ src/core/moduleManager.js
@@ -1,6 +1,7 @@
 /**
  * Torn Toolkit
  * Module Manager
+ * Version: 0.2.0
  */
 
 import Logger from "./logger.js";
@@ -10,44 +11,104 @@ export default class ModuleManager {
     constructor(storage) {
 
         this.storage = storage;
+
         this.modules = [];
 
     }
 
     register(module) {
 
-        const saved = this.storage.get(
+        const enabled = this.storage.get(
             `module.${module.id}`,
             module.enabled ?? true
         );
 
-        module.enabled = saved;
+        module.enabled = enabled;
 
         this.modules.push(module);
 
-        Logger.info(`Module loaded: ${module.name}`);
+        Logger.info(`Loaded module: ${module.name}`);
+
+    }
+
+    unregister(id) {
+
+        this.modules = this.modules.filter(
+            module => module.id !== id
+        );
+
+        Logger.warn(`Removed module: ${id}`);
+
+    }
+
+    get(id) {
+
+        return this.modules.find(
+            module => module.id === id
+        );
 
     }
 
     getAll() {
 
-        return this.modules;
+        return [...this.modules];
 
     }
 
-    isEnabled(id) {
+    getEnabled() {
 
-        const module = this.modules.find(m => m.id === id);
+        return this.modules.filter(
+            module => module.enabled
+        );
 
-        return module ? module.enabled : false;
+    }
+
+    enable(id) {
+
+        const module = this.get(id);
+
+        if (!module)
+            return false;
+
+        module.enabled = true;
+
+        this.storage.set(
+            `module.${id}`,
+            true
+        );
+
+        Logger.success(`${module.name} enabled`);
+
+        return true;
+
+    }
+
+    disable(id) {
+
+        const module = this.get(id);
+
+        if (!module)
+            return false;
+
+        module.enabled = false;
+
+        this.storage.set(
+            `module.${id}`,
+            false
+        );
+
+        Logger.warn(`${module.name} disabled`);
+
+        return true;
 
     }
 
     toggle(id) {
 
-        const module = this.modules.find(m => m.id === id);
+        const module = this.get(id);
 
-        if (!module) return;
+        if (!module)
+            return false;
 
         module.enabled = !module.enabled;
 
@@ -56,9 +117,33 @@ export default class ModuleManager {
             module.enabled
         );
 
-        Logger.success(
-            `${module.name}: ${module.enabled ? "Enabled" : "Disabled"}`
+        Logger.info(
+            `${module.name}: ${
+                module.enabled ? "Enabled" : "Disabled"
+            }`
         );
+
+        return module.enabled;
+
+    }
+
+    isEnabled(id) {
+
+        const module = this.get(id);
+
+        return module ? module.enabled : false;
+
+    }
+
+    count() {
+
+        return this.modules.length;
+
+    }
+
+    countEnabled() {
+
+        return this.getEnabled().length;
 
     }
 
