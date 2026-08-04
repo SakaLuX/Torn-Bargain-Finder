b6bfd4afa895859eded7c71473e4b641822ddaf9
SakaLuX <sakalutzu92@yahoo.com>
2026-08-04 16:14:41 +0300

    Update app.js

--- src/core/app.js
+++ src/core/app.js
@@ -1,1 +1,22 @@
--
+import { Logger } from "./logger.js";
+
+export class TornToolkit {
+
+    constructor() {
+
+        this.version = "0.1.0";
+
+    }
+
+    start() {
+
+        Logger.info("=======================================");
+        Logger.info("Starting Torn Toolkit...");
+        Logger.info("Version:", this.version);
+        Logger.info("Author: SakaLuX");
+        Logger.info("Repository: Torn-Toolkit");
+        Logger.info("=======================================");
+
+    }
+
+}
