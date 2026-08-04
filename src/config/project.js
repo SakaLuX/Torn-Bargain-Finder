e5909d9932714925aab414ac0f5dea814d1cbe5d
SakaLuX <sakalutzu92@yahoo.com>
2026-08-04 16:18:09 +0300

    Create config/project.js

---
+++ src/config/project.js
@@ -0,0 +1,19 @@
+export const PROJECT = {
+
+    name: "Torn Toolkit",
+
+    version: "0.1.0",
+
+    author: "SakaLuX",
+
+    github: "https://github.com/SakaLuX/Torn-Toolkit",
+
+    developer: {
+
+        name: "SakaLuX",
+
+        xid: 2380374
+
+    }
+
+};
