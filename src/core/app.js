8bce8ec45fd85a0fcf669d94ed253116289c4026
SakaLuX <sakalutzu92@yahoo.com>
2026-08-04 16:37:21 +0300

    Update app.js

--- src/core/app.js
+++ src/core/app.js
@@ -1,21 +1,34 @@
-import { Logger } from "./logger.js";
+import Logger from "./logger.js";
+import Storage from "./storage.js";
+import Router from "./router.js";
 
-export class TornToolkit {
+import Panel from "../ui/panel.js";
+
+export default class TornToolkit {
 
     constructor() {
 
         this.version = "0.1.0";
 
+        this.storage = new Storage();
+
+        this.panel = new Panel(this);
+
     }
 
     start() {
 
-        Logger.info("=======================================");
-        Logger.info("Starting Torn Toolkit...");
+        Logger.line();
+
+        Logger.info("Starting Torn Toolkit");
+
         Logger.info("Version:", this.version);
-        Logger.info("Author: SakaLuX");
-        Logger.info("Repository: Torn-Toolkit");
-        Logger.info("=======================================");
+
+        Logger.info("Current Page:", Router.current());
+
+        Logger.line();
+
+        this.panel.render();
 
     }
 
