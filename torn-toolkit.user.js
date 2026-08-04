66a973eb4a4ca524261cddd960b8170f0f0b7c7a
SakaLuX <sakalutzu92@yahoo.com>
2026-08-04 17:13:32 +0300

    Add userscript entry point

---
+++ torn-toolkit.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Torn Toolkit
+// @namespace    https://github.com/SakaLuX/Torn-Toolkit
+// @version      0.3.0
+// @description  Modern toolkit for Torn
+// @author       SakaLuX
+// @match        https://www.torn.com/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function () {
+
+    "use strict";
+
+    console.log("====================================");
+    console.log("🟢 Torn Toolkit");
+    console.log("Version: 0.3.0");
+    console.log("Author: SakaLuX");
+    console.log("====================================");
+
+})();
